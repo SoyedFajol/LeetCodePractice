930047b811e25886efa508547b04f8eae1edd988
Soyed Md. Solaman Fajul <solaman.fajul@bracits.com>
2025-05-27 18:35:52 +0600

    update

---
+++ 50SQL/Day04/1280 Students and Examinations.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT s.student_id,s.student_name,u.subject_name,count(e.subject_name) AS attended_exams
+FROM Students s
+CROSS JOIN Subjects u
+LEFT JOIN Examinations e
+ON s.student_id=e.student_id AND e.subject_name=u.subject_name
+GROUP BY s.student_id,s.student_name,u.subject_name
+ORDER BY s.student_id,s.student_name
